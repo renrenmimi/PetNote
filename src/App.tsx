26b6bdee810cf1131ea3efc5c4895f9351e2ca6b
WEIREN FENG <feng.weir@northeastern.edu>
2026-02-04 23:26:21 -0800

    Initial commit: project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-pink-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-pink-600">🐾 PetNote</h1>
+    </div>
+  );
+}
+
+export default App;
